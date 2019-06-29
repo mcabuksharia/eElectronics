e8f5810633da4c55432166cbbef845ef3e4f45f4
atul <atul2685@gmail.com>
2019-06-29 23:01:29 +0530

    devise gem added

--- app/controllers/electronic_controller.rb
+++ app/controllers/electronic_controller.rb
@@ -1,4 +1,5 @@
 class ElectronicController < ApplicationController
+	before_action :authenticate_user!
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
  # get 'electronic/index'
   root to: 'electronic#index', as: 'electronic'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
